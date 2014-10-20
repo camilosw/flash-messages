ed5bc6fb4388b94ef4d80a0eb47e6bbe3431c6f6
kmligue <liguekelvin@gmail.com>
2014-10-20 17:37:32 +0800

    added name, version, git on package

--- package.js
+++ package.js
@@ -1,7 +1,7 @@
 Package.describe({
   name: "kmligue:flash-messages",
   summary: "A package to display flash messages to the user.",
-  version: "1.0",
+  version: "1.0.0",
   git: "https://github.com/kmligue/flash-messages.git"
 });
 
